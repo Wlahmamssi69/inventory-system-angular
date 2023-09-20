44f8a8ded5d44d6bf189ac33b647dda313d3c2dc
Wlahmamssi69 <wafa.lahmamssi@outlook.com>
2023-09-20 21:07:55 +0100

    add login, signup, and the main dashboard structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageLoginComponent} from "./pages/page-login/page-login.component";
+import {PageInscriptionComponent} from "./pages/page-inscription/page-inscription.component";
+import {PageDashboardComponent} from "./pages/page-dashboard/page-dashboard.component";
+import {PageReportsComponent} from "./pages/page-reports/page-reports.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:PageLoginComponent
+  },
+  {
+    path:'inscription',
+    component:PageInscriptionComponent
+  },
+  {
+    path: '',
+    component:PageDashboardComponent,
+    children:[
+      {
+        path:'reports',
+        component:PageReportsComponent,
+      },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageReportsComponent } from './pages/page-reports/page-reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLoginComponent,
+    PageInscriptionComponent,
+    PageDashboardComponent,
+    PageReportsComponent
   ],
   imports: [
     BrowserModule,
