7d36218e96ad23e6461f42ff86c09d9134cbc924
Wlahmamssi69 <wafa.lahmamssi@outlook.com>
2023-09-23 01:19:00 +0100

    add-new-product-interface

---
+++ src/app/pages/products/new-product/new-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+@Component({
+  selector: 'app-new-product',
+  templateUrl: './new-product.component.html',
+  styleUrls: ['./new-product.component.scss']
+})
+export class NewProductComponent {
+faCircleInfo=faInfoCircle;
+}
